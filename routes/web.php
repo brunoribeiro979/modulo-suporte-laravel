67de3c7ecfbfc803ac3ea1079ca653842d6add21
Servidor Bubbles <bruno.santos979@yahoo.com.br>
2024-05-09 22:07:06 -0300

    Primeiro commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+
+use App\Http\Controllers\Admin\{SupportController};
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
